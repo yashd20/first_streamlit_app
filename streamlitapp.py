ac638fe8e375a69208c106f312408e7e09c54df5
yashd20 <68221011+yashd20@users.noreply.github.com>
2023-04-09 11:27:38 +0100

    Update streamlitapp.py
    Import pandas and show contents in streamlit dataframe

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,5 +1,7 @@
-
+import pandas
 import streamlit
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 streamlit.title("My Parent's New Healthy Diner")
 
@@ -10,3 +12,4 @@ streamlit.text("🐔 Hard-Boiled Free-Range Egg")
 streamlit.text("🥑🍞 Avaoado Toast")
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
