d5d16d43b9fccc4ad5b80e16b017403dbb7bb149
yashd20 <68221011+yashd20@users.noreply.github.com>
2023-04-10 22:29:42 +0100

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -17,6 +17,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("What fruit would you like information about?", list(my_fruit_list.index))
+streamlit.write(fruits_selected)
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 # Display the table on the page.
