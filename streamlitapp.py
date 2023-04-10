7af93efe6b4f354d7ff178fbd6d4c52940d5a2d0
yashd20 <68221011+yashd20@users.noreply.github.com>
2023-04-10 21:04:14 +0100

    Update streamlitapp.py
    Added snowflake connector query pulling from streamlit

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,6 +1,6 @@
 import pandas
 import streamlit
-import snowflake.connector
+
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -28,3 +28,12 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Shows json data as a dataframe
 streamlit.dataframe(fruityvice_normalized)
+
+
+import snowflake.connector
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
