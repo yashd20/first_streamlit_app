df29675a6e5e051b572fc98797dfa9d99a3104a6
yashd20 <68221011+yashd20@users.noreply.github.com>
2023-04-11 10:49:54 +0100

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -32,7 +32,7 @@ try:
   if not fruit_choice:
     streamlit.error('please select a fruit to get information.')
   else:
-    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/kiwi")
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
     
